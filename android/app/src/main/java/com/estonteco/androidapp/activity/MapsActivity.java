38b35b1d200927dc524d6bd444b8bd780581d997
toumash <mr.toumash@gmail.com>
2018-06-26 05:36:01 +0200

    fixes build

--- android/app/src/main/java/com/estonteco/androidapp/activity/MapsActivity.java
+++ android/app/src/main/java/com/estonteco/androidapp/activity/MapsActivity.java
@@ -1,5 +1,8 @@
 package com.estonteco.androidapp.activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.widget.Toast;
@@ -8,8 +11,10 @@ import com.estonteco.androidapp.R;
 import com.estonteco.androidapp.model.api.ParkingSlot;
 import com.estonteco.androidapp.model.api.ReservationAdded;
 import com.estonteco.androidapp.model.api.ReservationRequest;
+import com.estonteco.androidapp.network.NetworkServiceFactory;
 import com.estonteco.androidapp.network.ReservationService;
 import com.estonteco.androidapp.network.RetrofitClientInstance;
+import com.estonteco.androidapp.network.SlotService;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -32,6 +37,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     Map<Marker, ParkingSlot> markerMap;
     ReservationService reservationService = RetrofitClientInstance.getRetrofitInstance().create(ReservationService.class);
+    SlotService slotService = NetworkServiceFactory.createSlotService();
+    ReservationAdded mReservationAdded;
     private GoogleMap mMap;
     private List<ParkingSlot> mParkingSlots;
 
@@ -50,7 +57,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        Call<List<ParkingSlot>> call = reservationService.getAllParkingSlots();
+        Call<List<ParkingSlot>> call = slotService.getAllParkingSlots();
         call.enqueue(new Callback<List<ParkingSlot>>() {
             @Override
             public void onResponse(Call<List<ParkingSlot>> call, Response<List<ParkingSlot>> response) {
@@ -65,18 +72,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         markerMap.put(marker, r);
                     }
 
-                    // animate camera to last entry
-                    ParkingSlot last = mParkingSlots.get(mParkingSlots.size() - 1);
-                    LatLng latlong = new LatLng(last.DlugoscGeo, last.SzerokoscGeo);
-                    CameraPosition position = CameraPosition.builder().target(latlong).zoom(13).build();
-                    CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(position);
-                    mMap.animateCamera(cameraUpdate);
+                    if (!mParkingSlots.isEmpty()) {
+                        // animate camera to last entry
+                        ParkingSlot last = mParkingSlots.get(mParkingSlots.size() - 1);
+                        LatLng latlong = new LatLng(last.DlugoscGeo, last.SzerokoscGeo);
+                        CameraPosition position = CameraPosition.builder().target(latlong).zoom(13).build();
+                        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(position);
+                        mMap.animateCamera(cameraUpdate);
+                    }
                 }
             }
 
             @Override
             public void onFailure(Call<List<ParkingSlot>> call, Throwable t) {
-                //progressDoalog.dismiss();
+                //progressDialog.dismiss();
                 Toast.makeText(MapsActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
             }
         });
@@ -85,23 +94,46 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public boolean onMarkerClick(Marker marker) {
-        ParkingSlot selectedParkingSlot = markerMap.get(marker);
-        ReservationRequest request = new ReservationRequest();
-        request.reservationId = selectedParkingSlot.Id;
-        reservationService.book(request)
-                .enqueue(new Callback<ReservationAdded>() {
-                    @Override
-                    public void onResponse(Call<ReservationAdded> call, Response<ReservationAdded> response) {
-                        Toast.makeText(MapsActivity.this, "Reservation made successfully. Reservation ID=" + response.body().reservationId, Toast.LENGTH_LONG)
-                                .show();
-                    }
+        final ParkingSlot selectedParkingSlot = markerMap.get(marker);
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(MapsActivity.this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(MapsActivity.this);
+        }
+        builder.setTitle("Rezerwacja miejsca")
+                .setMessage("Czy chcesz zarezerwować miejsce parkingowe #" + selectedParkingSlot.Id)
+                .setNegativeButton(android.R.string.cancel, null)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        ReservationRequest request = new ReservationRequest();
+                        request.slotId = selectedParkingSlot.Id;
+                        reservationService.book(request)
+                                .enqueue(new Callback<ReservationAdded>() {
+                                    @Override
+                                    public void onResponse(Call<ReservationAdded> call, Response<ReservationAdded> response) {
+                                        mReservationAdded = response.body();
+                                        if (mReservationAdded != null) {
+                                            Toast.makeText(MapsActivity.this,
+                                                    "Reservation made successfully. Reservation ID=" + mReservationAdded.reservationId,
+                                                    Toast.LENGTH_LONG).show();
+                                        }else{
+                                            Toast.makeText(MapsActivity.this, "ERROR", Toast.LENGTH_LONG)
+                                                    .show();
+                                        }
+                                    }
 
-                    @Override
-                    public void onFailure(Call<ReservationAdded> call, Throwable t) {
-                        Toast.makeText(MapsActivity.this, "ERROR", Toast.LENGTH_LONG)
-                                .show();
+                                    @Override
+                                    public void onFailure(Call<ReservationAdded> call, Throwable t) {
+                                        Toast.makeText(MapsActivity.this, "ERROR", Toast.LENGTH_LONG)
+                                                .show();
+                                    }
+                                });
                     }
-                });
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+
         return false;
     }
 }
